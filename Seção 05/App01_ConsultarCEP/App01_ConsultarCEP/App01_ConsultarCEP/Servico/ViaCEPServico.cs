35848e1b275930b5b5c8af2ca2e9e001e01d848e
programadormovel <programadormovel@hotmail.com>
2018-05-25 18:55:50 -0300

    Seção 06 - Completa

--- Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Servico/ViaCEPServico.cs
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Servico/ViaCEPServico.cs
@@ -11,15 +11,18 @@ namespace App01_ConsultarCEP.Servico
     {
         private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
 
-        string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
 
-        WebClient wc = new WebClient();
-        string Conteudo = wc.DownloadString(NovoEnderecoURL);
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
 
-        Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
 
             if (end.cep == null) return null;
 
             return end;
+        }
     }
 }
