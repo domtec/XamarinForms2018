d00a7a9e040fa3711f91f26e0395ee48146761b7
programadormovel <programadormovel@hotmail.com>
2018-05-27 02:48:49 -0300

    Controles até SearchBar

---
+++ Seção 08/App01_ControleXF/App01_ControleXF/App01_ControleXF/Controles/ProgressBarPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Controles
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ProgressBarPage : ContentPage
+	{
+		public ProgressBarPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Modificar(object sender, EventArgs args)
+        {
+            Bar1.Progress = 1;
+            Bar2.ProgressTo(1, 5000, Easing.Linear);
+            Bar3.ProgressTo(1, 5000, Easing.SpringIn);
+            Bar4.ProgressTo(1, 5000, Easing.SpringOut);
+            Bar5.ProgressTo(1, 5000, Easing.CubicIn);
+            Bar6.ProgressTo(1, 5000, Easing.CubicInOut);
+            Bar7.ProgressTo(1, 5000, Easing.CubicOut);
+            Bar8.ProgressTo(1, 5000, Easing.BounceIn);
+            Bar9.ProgressTo(1, 5000, Easing.BounceOut);
+            Bar10.ProgressTo(1, 5000, Easing.SinIn);
+            
+        }
+	}
+}
---
+++ Seção 08/App01_ControleXF/App01_ControleXF/App01_ControleXF/Menu/Master.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Menu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Master : MasterDetailPage
+	{
+		public Master ()
+		{
+			InitializeComponent ();
+		}
+        private void GoActivityIndicatorPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.ActivityIndicatorPage();
+            IsPresented = false;
+        }
+
+        private void GoProgressBarPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.ProgressBarPage();
+            IsPresented = false;
+        }
+
+        private void GoBoxViewPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.BoxViewPage();
+            IsPresented = false;
+        }
+
+        private void GoLabelPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.LabelPage();
+            IsPresented = false;
+        }
+
+        private void GoButtonPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.ButtonPage();
+            IsPresented = false;
+        }
+        private void GoEntryEditorPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.EntryEditorPage();
+            IsPresented = false;
+        }
+
+        private void GoDatePickerPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.DatePickerPage();
+            IsPresented = false;
+        }
+
+        private void GoTimePickerPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.TimePickerPage();
+            IsPresented = false;
+        }
+
+        private void GoPickerPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.PickerPage();
+            IsPresented = false;
+        }
+
+        private void GoSearchBarPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.SearchBarPage();
+            IsPresented = false;
+        }
+    }
+}
