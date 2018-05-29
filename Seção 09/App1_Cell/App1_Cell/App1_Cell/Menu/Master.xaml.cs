a8fa22e6a80324e798cdeffe611505847534fb3a
programadormovel <programadormovel@hotmail.com>
2018-05-29 09:44:31 -0300

    Estudo de Células e ListView

---
+++ Seção 09/App1_Cell/App1_Cell/App1_Cell/Menu/Master.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1_Cell.Menu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Master : MasterDetailPage
+	{
+		public Master ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoPagina1(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pagina.TextCellPage());
+            IsPresented = false;
+        }
+
+        private void GoPagina2(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pagina.ImageCellPage());
+            IsPresented = false;
+        }
+
+        private void GoPagina3(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pagina.EntryCellPage());
+            IsPresented = false;
+        }
+
+        private void GoPagina4(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pagina.SwitchCellPage());
+            IsPresented = false;
+        }
+
+        private void GoPagina5(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pagina.ViewCellPage());
+            IsPresented = false;
+        }
+
+        private void GoPagina6(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Pagina.ListViewPage());
+            IsPresented = false;
+        }
+
+        private void GoPagina7(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Pagina.ListViewButtonPage());
+            IsPresented = false;
+        }
+    }
+}
